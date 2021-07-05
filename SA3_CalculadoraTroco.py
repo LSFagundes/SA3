b2e30d6bf2ef56c0b1ce439a86a909d99e55c927
LSFagundes <leonardo.s.fagundes@gmail.com>
2021-07-05 09:00:57 -0300

    Update SA3_CalculadoraTroco.py

--- SA3_CalculadoraTroco.py
+++ SA3_CalculadoraTroco.py
@@ -1,11 +1,11 @@
 from decimal import Decimal
     
-compra =  Decimal (input("Valor da compra: "))
-pagamento = Decimal (input("Valor pago: "))
+compra =  Decimal (input("Valor Total das Compras: "))
+pagamento = Decimal (input("Valor Recebido: "))
 troco = pagamento - compra
-print("Total do troco: R$", troco)
+print("Troco: R$", troco)
 print("Você deve entregar ao cliente:")
-ced = Decimal(100)
+ced = Decimal('100.00')
 TotalCedulas = Decimal(0)
 while True:
     if troco >= ced:
@@ -16,22 +16,22 @@ while True:
             break
     else:
         if ced > 1 and TotalCedulas > 0:
-            print(TotalCedulas, "cédula(s) de R$", ced)
+            print(TotalCedulas, "Nota(s): R$", ced)
         if ced <=1 and TotalCedulas > 0:
-             print (TotalCedulas, "moeda(s) de R$ ", ced)
-        if ced == 100.:
-            ced = Decimal(50)
-        if ced == 50:
-             ced = Decimal(20)
-        elif ced == 20:
-             ced = Decimal(10)
-        elif ced == 10:
-             ced = Decimal(2)
-        elif ced == 2:
-             ced = Decimal(1)
-        elif ced == 1:
-             ced = Decimal(0.5)
-        elif ced == 0.5:
+             print (TotalCedulas, "Moeda(s): R$ ", ced)
+        if ced == 100.00:
+           ced = Decimal('50.00')
+        if ced == 50.00:
+             ced = Decimal('20.00')
+        elif ced == 20.00:
+             ced = Decimal('10.00')
+        elif ced == 10.00:
+             ced = Decimal('2.00')
+        elif ced == 2.00:
+             ced = Decimal('1.00')
+        elif ced == 1.00:
+             ced = Decimal('0.50')
+        elif ced == 0.50:
              ced = Decimal('0.05')
         TotalCedulas = 0
         if troco == 0:
